a1cf55b61f35176a348af59c09f76ae3f6d8cecb
RahumaFathi <it19172910@my.sliit.lk>
2022-10-30 22:41:16 +0530

    Edit

--- Teacher/View/header1.php
+++ Teacher/View/header1.php
@@ -1,48 +1,53 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <title>Admin Login</title>
     <style>
-		<?php include '../Style/login.css'; ?>
-	</style>
+    <?php include '../Style/login.css';
+    ?>
+    </style>
     <style media="screen">
-    #error_messege{
-      color: White;
-      font-weight: bold;
-      margin-bottom: 20px;
-      padding: 0px;
-      background: #de0404;
-      text-align: center;
-      font-size: 18px;
-      transition: all 0.5s ease;
+    #error_messege {
+        color: White;
+        font-weight: bold;
+        margin-bottom: 20px;
+        padding: 0px;
+        background: #de0404;
+        text-align: center;
+        font-size: 18px;
+        transition: all 0.5s ease;
     }
     </style>
 
-     <!-- <link rel="stylesheet" href="application_resources/javascripts/plugins/bootstrap/css/bootstrap.min.css"> -->
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="application_resources/javascripts/plugins/bootstrap/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- fontawesome css -->
 
-<script src="../Script/adminloginVal.js"></script>
-  </head>
-  <body>
+    <script src="../Script/adminloginVal.js"></script>
+</head>
+
+<body>
     <div class="main-content"></div>
 
-  <div class="header-content">
-    <div class="school-logo-title">
-        <div class="schoolLogo">
-        <a href="../../Home/index.html">
-            <img alt="" src="../images/schoolLogo.png" height="90px">
-  </a>
-        </div>
-        <div class="school-title">
-            <span class="school-title-name" >Vidyadarsha Maha Vidyalaya</span>
-            <span class="school-sub-title" >School Content Management System</span>
-        </div>
-        <div class="back-button">
-            <button  ><a href="../../Home/index.html">Back</a></button>
+    <div class="header-content">
+        <div class="school-logo-title">
+            <div class="schoolLogo">
+                <a href="../../Home/index.html">
+                    <img alt="" src="../images/schoolLogo.png" height="90px">
+                </a>
+            </div>
+            <div class="school-title">
+                <span class="school-title-name">Vidyadarsha Maha Vidyalaya</span>
+                <span class="school-sub-title">School Content Management System</span>
+            </div>
+            <div class="back-button">
+                <button><a href="../../Home/index.php">Back</a></button>
+            </div>
         </div>
     </div>
-    </div>
-  </body>
+</body>
+
 </html>
