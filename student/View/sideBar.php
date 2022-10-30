76923060e26632ead69e626b0fa083ff7c6db4b6
charithdhananjaya <charithdhananjaya01@gmail.com>
2022-10-30 08:59:02 +0530

    commit 1

--- student/View/sideBar.php
+++ student/View/sideBar.php
@@ -1,9 +1,39 @@
-<tr>
-        <td width="250px">
-          <h3>My Account</h3>
-          <hr>
-          <ul>
-            <li><a href="dashboard.php">Dashboard</a></li>  
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <style>
+		<?php include '../Style/Side-bar.css'; ?>
+	</style>
+    <style media="screen">
+    #error_messege{
+      color: White;
+      font-weight: bold;
+      margin-bottom: 20px;
+      padding: 0px;
+      background: #de0404;
+      text-align: center;
+      font-size: 18px;
+      transition: all 0.5s ease;
+    }
+    </style>
+
+     <!-- <link rel="stylesheet" href="application_resources/javascripts/plugins/bootstrap/css/bootstrap.min.css"> -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+       <!-- fontawesome css -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<script src="../Script/adminloginVal.js"></script>
+  </head>
+  <body>
+<div class="sidebar-content">
+
+  <div class="single-sidebar-widget hidden-md hidden-sm hidden-xs">
+
+      
+                        <div class="special-links">
+
+                        <li><a href="dashboard.php">Dashboard</a></li>  
             <li><a href="routine.php">Class Routine</a></li>
             <li><a href="viewprofile.php">View Profile</a></li>
             <li><a href="teachernotice.php">Teachers Notice</a></li>
@@ -18,6 +48,9 @@
             <li><a href="requestforbook.php">Request of Books</a></li>
             <li><a href="issuebook.php">Issue Book History</a></li>
             <li><a href="booksinfo.php">Information of Books</a></li>
+                        </div>
+                    </div>
 
-          </ul>
-        </td>
+                    </div>
+  </body>
+</html>
